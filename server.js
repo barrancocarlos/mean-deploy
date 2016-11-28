d1e13b681120dfad54ac2aec842d347f629e348b
Carlos <cbarranco@decktra.com>
2016-11-28 19:49:34 -0400

    mongoose

--- server.js
+++ server.js
@@ -14,7 +14,7 @@
 
     // configuration =================
 
-     connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/' + process.env.OPENSHIFT_APP_NAME;
+     var connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/' + process.env.OPENSHIFT_APP_NAME;
      mongoose.connect('mongodb://' + connection_string);     // connect to mongoDB database 
 
     app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
